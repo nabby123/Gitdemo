b83586ad81df5aa711f9f07e971278b2012c472a
nabby123 <mohanty.nabby@gmail.com>
2020-11-27 04:12:41 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	
+	@Test
+	public void Androidapp()
+	{
+	System.out.println("Androidapp");	
+	}
+	
+	
+	@Test
+	public void iosapp()
+	{
+	System.out.println("iosapp");	
+	}
+	
+
+}
